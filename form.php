79e1e610a8b66e1ca9e70f83b8bbf168a9d89a29
Typhene Benfriha <typh94@live.fr>
2025-03-01 11:54:23 -0800

    Initial commit

---
+++ form.php
@@ -0,0 +1,4 @@
+Last Name : <?php echo htmlspecialchars($_POST['lname']);  ?><br>
+First Name : <?php echo htmlspecialchars($_POST['fname']);?><br>
+Message : <?php echo htmlspecialchars($_POST['message']);  ?><br>
+
