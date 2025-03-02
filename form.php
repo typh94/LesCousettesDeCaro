fc1c702bc8961061e960b857ae28d669a81d0814
Typhene Benfriha <typh94@live.fr>
2025-03-01 17:32:04 -0800

    Removing form.php, setupConnection.php, test.sql from master branch

--- form.php
+++
@@ -1,4 +0,0 @@
-Last Name : <?php echo htmlspecialchars($_POST['lname']);  ?><br>
-First Name : <?php echo htmlspecialchars($_POST['fname']);?><br>
-Message : <?php echo htmlspecialchars($_POST['message']);  ?><br>
-
