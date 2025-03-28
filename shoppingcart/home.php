6806891ebfee6e682fcd28902635fe40f16e0037
Typhene Benfriha <132304064+typh94@users.noreply.github.com>
2025-03-27 20:08:58 -0500

    added PHP shopping cart related files

---
+++ shoppingcart/home.php
@@ -0,0 +1,33 @@
+
+<?php
+// Get the 4 most recently added products
+$stmt = $pdo->prepare('SELECT * FROM products ORDER BY date_added DESC LIMIT 4');
+$stmt->execute();
+$recently_added_products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+<?=template_header(title: 'Home')?>
+
+<div class="featured" style="background-image: url('imgs/banner22.png'); ">
+    <br><br>
+ </div>
+<div class="recentlyadded content-wrapper">
+    <h2>Recently Added Products</h2>
+    <div class="products">
+        <?php foreach ($recently_added_products as $product): ?>
+        <a href="index.php?page=product&id=<?=$product['id']?>" class="product">
+            <img src="imgs/<?=$product['img']?>" width="200" height="200" alt="<?=$product['title']?>">
+            <span class="name"><?=$product['title']?></span>
+            <span class="price">
+                &dollar;<?=$product['price']?>
+                <?php if ($product['rrp'] > 0): ?>
+                <span class="rrp">&dollar;<?=$product['rrp']?></span>
+                <?php endif; ?>
+            </span>
+        </a>
+        <?php endforeach; ?>
+    </div>
+</div>
+
+<?=template_footer()?>
+ 
+
